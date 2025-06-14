66a0d7c414569dbfad4e911cb85d7de237f4356d
Alexsandr <82975768+Sminor01@users.noreply.github.com>
2025-06-14 23:58:08 +0300

    add js

---
+++ index.js
@@ -0,0 +1,55 @@
+const model = [
+    {type: 'title', value: 'Hello World form JS'},
+    {type: 'text', value : 'here we go with some text'},
+    {type: 'columns', value: [
+        '111111',
+        '222222',
+        '333333'
+    ]}
+]
+
+const $site = document.querySelector('#site')
+
+const title = (block) => {
+        return `
+        <div class="row">
+            <div class="col-sm">
+                <h1>${block.value}</h1>
+            </div>
+        </div>
+    `
+}
+
+const text = (block) => {
+    return `
+            <div class="row">
+                <div class="col-sm">
+                    <p>${block.value}</p>
+                </div>
+            </div>
+        `
+}
+
+model.forEach(block => {
+    let html = ''
+
+    if (block.type === 'title') {
+        html = title(block)
+    } else if (block.type === 'text') {
+        html = text(block)
+    } else if (block.type === 'columns') {
+        html = columns(block)
+    }
+
+    $site.insertAdjacentHTML('beforeend', html)
+})
+
+function columns (block) {
+    const html = block.value.map(item => `<div class="col-sm">${item}</div>`)
+    return `
+        <div class="row">
+            ${html.join('')}
+        </div>
+    `
+}
+
